7a893b43be0cfef5a7b09c4a2508414d154fef57
nangdi <zovl5546@naver.com>
2023-09-08 00:29:15 +0900

    9/7 study

--- sw expert academy/diffi3/15868.py
+++ sw expert academy/diffi3/15868.py
@@ -4,3 +4,12 @@ for i in range(1, t+1):
     xor = []
     for _ in range(n):
         xor.append(list(map(int, input())))
+    c = 0
+    for j in range(m):
+        if xor[0][j] == xor[1][j]:
+            print(f"#{i} no")
+            c += 1
+            break
+    if c == 1:
+        continue
+    print(f"#{i} yes")
